8f06533e4fcfe242e20d6bcdf7fce30d6f7e852b
eichild <luucasgregorio@gmail.com>
2022-01-31 23:43:01 -0300

    criado repository controller e model de aplicação

---
+++ simpleapi/src/main/java/eichild/simpleapi/controller/UsuarioController.java
@@ -0,0 +1,30 @@
+package eichild.simpleapi.controller;
+
+import eichild.simpleapi.model.Usuario;
+import eichild.simpleapi.repository.UsuarioRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/usuario")
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @GetMapping("/")
+    public List<Usuario> findAll(){
+        var usuario = usuarioRepository.findAll();
+        return usuario
+                .stream()
+                .map(Usuario::converter)
+                .collect(Collectors.toList());
+    }
+}
---
+++ simpleapi/src/main/java/eichild/simpleapi/model/Usuario.java
@@ -0,0 +1,53 @@
+package eichild.simpleapi.model;
+
+import eichild.simpleapi.repository.UsuarioRepository;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cod_pessoa")
+    private Long cod_usuario;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    public static Usuario converter(Usuario u){
+        var usuario = new Usuario();
+        usuario.setCod_usuario(u.getCod_usuario());
+        usuario.setNome(u.getNome());
+        usuario.setEmail(u.getEmail());
+        return usuario;
+    }
+
+    public Long getCod_usuario() {
+        return cod_usuario;
+    }
+
+    public void setCod_usuario(Long cod_usuario) {
+        this.cod_usuario = cod_usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
